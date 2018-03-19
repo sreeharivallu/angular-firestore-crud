5bf9c661240ccd9fbec3b173d8e308acea6d25b0
sreehari <sree.bobby4u@gmail.com>
2018-03-19 21:39:14 +0530

    budget5 first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { TileComponent } from './tile/tile.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    TileComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
